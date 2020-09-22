c55f5c272ce9cbbbc729221bbeadf5221c6f5113
kwkohcool <kwkoh_cool@hotmail.com>
2020-09-22 15:22:37 +0900

    add note interface

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -3,3 +3,12 @@ export type NotePitch = "A" | "B" | "C" | "D" | "E" | "F" | "G";
 export type OctaveIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type MidiValue = number;
 export type PitchIndex = number;
+
+export interface Note {
+  midi: MidiValue;
+  type: NoteType;
+
+  pitch: NotePitch;
+  index: PitchIndex;
+  octave: OctaveIndex;
+}
