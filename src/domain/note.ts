afaecc1978960d86b11235207682bfa32baf05e0
kwkohcool <kwkoh_cool@hotmail.com>
2020-09-23 19:00:42 +0900

    more on note

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -4,6 +4,14 @@ export type OctaveIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type MidiValue = number;
 export type PitchIndex = number;
 
+const C1_MIDI_NUMBER = 24;
+const C4_MIDI_NUMBER = 60;
+const B5_MIDI_NUMBER = 83;
+
+export const LOWER_NOTE = C4_MIDI_NUMBER;
+export const HIGHER_NOTE = B5_MIDI_NUMBER;
+export const SEMITONES_IN_OCTAVE = 12;
+
 export interface Note {
   midi: MidiValue;
   type: NoteType;
@@ -12,3 +20,19 @@ export interface Note {
   index: PitchIndex;
   octave: OctaveIndex;
 }
+
+export const NATURAL_PITCH_INDICES: PitchIndex[] = [0, 2, 4, 5, 7, 9, 11];
+export const PITCHES_REGISTRY: Record<PitchIndex, NotePitch> = {
+  0: "C",
+  1: "C",
+  2: "D",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "F",
+  7: "G",
+  8: "G",
+  9: "A",
+  10: "A",
+  11: "B",
+};
